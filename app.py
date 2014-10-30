a3e872fcf5febc0ddc1b32d73452d52cbccf60b8
Tyler Lubeck <tyler@tylerlubeck.com>
2014-10-29 22:38:17 -0400

    remove redundent

--- app.py
+++ app.py
@@ -46,13 +46,9 @@ def APS():
     lid = -1
     try:
         data = request.get_json()
-        data["lid"]
-        data["APS"]
         lid = cur.execute("""SELECT id from location where id=%s""", (int(data['lid']),))
         if not lid:
             raise "ERROR"
-        item['mac']
-        item['strength']
         for item in data["APS"]:
             cur.execute("""INSERT into accesspoint (MAC, strength, location_id, recorded
                 VALUES ( %s, %s, %s, NOW() )""", (item['MAC'], item['strength'],lid,))
