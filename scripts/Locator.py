17709dd5f948d3668dfcc877fbc2bbd60a0bfe68
Tyler Lubeck <tyler@tylerlubeck.com>
2015-01-29 21:02:30 -0500

    Clean up Database file

--- scripts/Locator.py
+++ scripts/Locator.py
@@ -1,19 +1,22 @@
 #   This Script:
 #   - Currently:
-#       - Receive Point (x,y) from Database
+#       - Receive Point (x, y) from Database
 #       - Draws that point on Halligan Floor 2
 #   - Future:
 #       - Receive Floor Plan Name from Database
 #       - Draw Point on Floor Plan File
 
-from PIL import Image, ImageDraw
-from db.db import cur
+from PIL import Image,  ImageDraw
+from db.db import Database
 import os
+import argparse
 
 
-def main():
-    halligan_two = os.path.join(os.getcwd(), 'Halligan_2.png')
-    cur.execute("""SELECT x,y from demhoes order by id desc limit 1 """)
+def Launch_Locator(password):
+    db = Database(password)
+    cur = db.get_cur()
+    halligan_two = os.path.join(os.getcwd(),  'Halligan_2.png')
+    cur.execute("""SELECT x, y from demhoes order by id desc limit 1 """)
     row = cur.fetchone()
     if row is not None:
         x = int(float(row[0]))
@@ -21,11 +24,17 @@ def main():
 
     image = Image.open(halligan_two)
 
-    radius = 5 # point radius
+    radius = 5  # point radius
     draw_image = ImageDraw.Draw(image)
-    draw_image.ellipse((x-radius,y-radius,x+radius,y+radius), fill='blue',outline='red')
+    draw_image.ellipse((x-radius, y-radius, x+radius, y+radius),
+                       fill='blue', outline='red')
     image.show()
     image.close()
 
+
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('db_password', help='The database password')
+    args = parser.parse_args()
+
+    Launch_Locator(args.db_password)
--- scripts/db/db.py
+++ scripts/db/db.py
@@ -1,68 +1,70 @@
-
 import pymysql
-import os
-#insert your sql information here
-password = os.environ.get('SIRIUS_PASSWORD')
-if password is None:
-    raise Exception('Could not get database password')
-conn = pymysql.connect(host='seniorindoorlocation.chopksxzy4yo.us-east-1.rds.amazonaws.com', db='sirius', user='wormtail', passwd=password)
-conn.autocommit(True)
 
-cur = conn.cursor()
 DEBUG = False
 
 SERVER_URL = "http://mapbuilder.herokuapp.com/"
 if DEBUG:
-  SERVER_URL = "http://localhost:5000/"
-
-def create_location():
-    cur.execute("""DROP TABLE if exists location;""")
-    cur.execute("""CREATE TABLE location(
-      id INT NOT NULL AUTO_INCREMENT,
-      PRIMARY KEY(id),
-      name TEXT,
-      verbose_name TEXT,
-      x INT,
-      y INT,
-      direction INT,
-      floor_id INT
-    );""")
-
-def create_AP():
-    cur.execute("""DROP TABLE if exists accesspoint;""")
-    cur.execute("""CREATE TABLE accesspoint (
-      id INT NOT NULL AUTO_INCREMENT,
-      PRIMARY KEY(id),
-      MAC TEXT,
-      strength DOUBLE,
-      std_dev FLOAT,
-      location_id INT,
-      recorded DATETIME
-    );""")
-
-def create_floor():
-    cur.execute("""DROP TABLE if exists floor;""")
-    cur.execute("""CREATE TABLE floor (
-      id INT NOT NULL AUTO_INCREMENT,
-      PRIMARY KEY(id),
-      imagePath TEXT,
-      floor INT,
-      building TEXT
-    );""")
-
-def create_demhoes():
-    cur.execute("""DROP TABLE if exists demhoes;""")
-    cur.execute("""CREATE TABLE demhoes (
-      id INT NOT NULL AUTO_INCREMENT,
-      PRIMARY KEY(id),
-      x TEXT,
-      y INT,
-      recorded DATETIME
-    );""")
+    SERVER_URL = "http://localhost:5000/"
 
 
-def create_all_tables():
-    create_location()
-    create_AP()
-    create_floor()
+class Database:
+    def __init__(self, password):
+        self.password = password
+        self.cur = self.get_cur()
 
+    def get_cur(self):
+        host = 'seniorindoorlocation.chopksxzy4yo.us-east-1.rds.amazonaws.com'
+        conn = pymysql.connect(host=host, db='sirius',
+                               user='wormtail', passwd=self.password)
+        conn.autocommit(True)
+        return conn.cursor()
+
+    def create_location(self):
+        self.cur.execute("""DROP TABLE if exists location;""")
+        self.cur.execute("""CREATE TABLE location(
+        id INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY(id),
+        name TEXT,
+        verbose_name TEXT,
+        x INT,
+        y INT,
+        direction INT,
+        floor_id INT
+        );""")
+
+    def create_AP(self):
+        self.cur.execute("""DROP TABLE if exists accesspoint;""")
+        self.cur.execute("""CREATE TABLE accesspoint (
+        id INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY(id),
+        MAC TEXT,
+        strength DOUBLE,
+        std_dev FLOAT,
+        location_id INT,
+        recorded DATETIME
+        );""")
+
+    def create_floor(self):
+        self.cur.execute("""DROP TABLE if exists floor;""")
+        self.cur.execute("""CREATE TABLE floor (
+        id INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY(id),
+        imagePath TEXT,
+        floor INT,
+        building TEXT
+        );""")
+
+    def create_demhoes(self):
+        self.cur.execute("""DROP TABLE if exists demhoes;""")
+        self.cur.execute("""CREATE TABLE demhoes (
+        id INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY(id),
+        x TEXT,
+        y INT,
+        recorded DATETIME
+        );""")
+
+    def create_all_tables(self):
+        self.create_location()
+        self.create_AP()
+        self.create_floor()
