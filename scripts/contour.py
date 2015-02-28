0905edbf396f42d1dbdc40f70e6e879b8970ceb0
dgriff03 <daniel.griffin@tufts.edu>
2015-02-27 19:00:30 -0500

    working

--- scripts/contour.py
+++ scripts/contour.py
@@ -2,7 +2,7 @@ from db.db import Database
 from sys import argv, exit
 import os
 from PIL import Image
-from mpl_toolkits.mplot3d import Axes3D
+import math
 from matplotlib import cm
 import matplotlib.pyplot as plt
 import StringIO
@@ -12,7 +12,7 @@ import StringIO
 
 my_dpi = 100
 
-BLOCK_SIZE = 8
+BLOCK_SIZE = 50
 
 SHOW = False # Determines if images are shown or merely saved
 
@@ -30,12 +30,13 @@ cur = db.get_cur()
 
 path_offset = "../Static_Files/" # Path offset from cwd to image directory
 
+def dist(x1,y1,x2,y2):
+    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
 
-def overlay_image(img,data):
+
+def overlay_image(img,data,zero=0):
     width,height = img.size
-    contourImage,legendImage = make_contour_map(data, height,width)
-    contourImage = contourImage.crop( crop_box(contourImage) )
-    contourImage = contourImage.resize((width,height), Image.BILINEAR)
+    contourImage,legendImage = make_contour_map(data, height,width,zero)
     overlay = Image.new("RGB", (width, height), "white")
     overlayPixels = overlay.load()
     contourPixels = contourImage.load()
@@ -45,13 +46,13 @@ def overlay_image(img,data):
         for h in range(height):
             r,g,b = mapPixels[w,h]
             if r <= THRESHOLD and g <= THRESHOLD and b <= THRESHOLD:
-                overlayPixels[w,h] = (0,0,0)
+                overlayPixels[w,h] = (255,255,255)
             else:
                 if w < cw and h < ch:
                     overlayPixels[w,h] = contourPixels[w,h]
                 else:
                     overlayPixels[w,h] = mapPixels[w,h]
-    return overlay
+    return overlay, legendImage
 
 
 def crop_box(img):
@@ -83,20 +84,53 @@ def current_fig_image():
     plt.clf()
     return img
 
-def rectify_data(data):
+def rectify_data(data,zero=0):
     """Assumes data is a proper 2D array"""
     if len(data) == 0:
         return []
     rows = len(data)
-    cols = len(data[0]) 
+    cols = len(data[0])
     new_data = [[0 for j in range(cols)] for i in range(rows)]
     for row in range(rows):
         for col in range(cols):
-            new_data[row][col] = block_average(data,row,col,BLOCK_SIZE, cols,rows)
+            new_data[row][col] = block_average(data,row,col,BLOCK_SIZE, cols,rows,zero)
     return new_data
 
 
-def block_average(data,row,col,BLOCK_SIZE,width,height):
+def weighted_avg(L,zero=0):
+    #[(dist,value),...]
+    if not L:
+        return zero
+    s = float(sum([x[0] for x in L]))
+    return sum( [x[1] / s for x in L] )
+
+
+def rectify_data_dist(data,zero=0):
+    """Assumes data is a proper 2D array"""
+    if len(data) == 0:
+        return []
+    rows = len(data)
+    cols = len(data[0])
+    new_data = [[zero for j in range(cols)] for i in range(rows)]
+    dist_data = [[[] for j in range(cols)] for i in range(rows)]
+
+    for row in range(rows):
+        for col in range(cols):
+            if data[row][col] == 0:
+                for r in range(rows):
+                    for c in range(cols):
+                        if r != row and c != col:
+                            dist_data[row][col].append((dist(row,col,r,c), data[r][c]))
+                new_data[row][col] = weighted_avg(dist_data[row][col],zero)
+            else:
+                new_data[row][col] = data[row][col]
+    return new_data
+
+
+
+
+
+def block_average(data,row,col,BLOCK_SIZE,width,height,zero=0):
     """averages the values in data in a block of side size BLOCK_SIZE."""
     r = BLOCK_SIZE // 2
     s = 0
@@ -107,11 +141,11 @@ def block_average(data,row,col,BLOCK_SIZE,width,height):
                 s += data[i][j]
                 counter += 1
     if counter == 0:
-        return -100
+        return zero
     return float(s) / counter
 
  
-def make_contour_map(data,height,width):
+def make_contour_map(data,height,width,zero=0):
     """Takes the given data and builds the csv string. 
     This function DOES NOT save the file. The expected input data
     should be a list in the format [(x,y,z),...]. The y data will be
@@ -120,41 +154,29 @@ def make_contour_map(data,height,width):
     xs = sorted(set([item[0] for item in data]))
     ys = sorted(set([height - item[1] for item in data]))
     Z = [item[2] for item in data]
-    xs.insert(0,0)
-    ys.insert(0,0)
-    Z.insert(0,0)
-    xs.append(width)
-    ys.append(height)
-    Z.append(0)
     min_z = 0
     #Flips ys
     num_X = len(xs)
     num_Y = len(ys)
-    zs = [[min_z for j in range(num_X)] for i in range(num_Y)]
+    zs = [[min_z for j in range(width)] for i in range(height)]
         #indexed row column
-    lines = []
-    lines.append("{},{}".format(num_Y,num_X))
-    x_pos = {} # dictionary from x value to x index
-    y_pos = {} # dictionary from y value to y index
-    for i,y in enumerate(ys):
-        y_pos[height - y] = i #initial y value, not flipped
-        lines.append(str(y))
-    for i,x in enumerate(xs):
-        x_pos[x] = i
-        lines.append(str(x))
-    for (x,y,z) in data:
-        zs[ y_pos[y] ][ x_pos[x] ] = z
-    zs = rectify_data(zs)
-    for line in zs:
-        lines.append( ','.join( map(str,line) ) )
+    for (dx,dy,dz) in data:
+        zs[height - dy][dx] = dz
+
+    zs = rectify_data(zs,zero)
     plt.gca().set_ylim([0,height])
     plt.gca().set_xlim([0,width])
     plt.gcf().set_frameon(False)
-    cs = plt.contourf(xs,ys,zs)
-    labels = map(lambda x: str(x) + " -db" ,cs.levels)
+    cs = plt.contourf([x for x in range(width)],[y for y in range(height)],zs)
+    if zero == 0:
+        labels = map(lambda x: str(x) + " APs" ,cs.levels)
+    else:   
+        labels = map(lambda x: str(x) + " -db" ,cs.levels)
     proxy = [plt.Rectangle((0,0),1,1,fc = pc.get_facecolor()[0]) 
         for pc in cs.collections]
     contourImage = current_fig_image()
+    contourImage = contourImage.crop( crop_box(contourImage) )
+    contourImage = contourImage.resize((width,height), Image.BILINEAR)
     plt.legend(proxy, labels)
     legendImage = current_fig_image()
     legendImage = legendImage.crop( crop_box(legendImage) )
@@ -185,25 +207,33 @@ def main():
         join accesspoint on location.id=location_id
         where floor_id=%s
         group by location.id""",[floorId])
-    overlay_image(img,cur.fetchall()).show()
+    imgCon, leg = overlay_image(img,cur.fetchall(),0)
+    imgCon.save(os.path.join(os.getcwd(),path_offset,"floor_{}_AP_map.jpg".format(floorId) ) )
+    leg.save(os.path.join(os.getcwd(),path_offset,"floor_{}_AP_legend.jpg".format(floorId) ) )
     #Maximim strength map
     cur.execute("""select x,y,max(strength) from location 
         join accesspoint on location.id=location_id
         where floor_id=%s
         group by location.id""",[floorId])
-    overlay_image(img,cur.fetchall()).show()
+    imgCon, leg = overlay_image(img,cur.fetchall(),-120)
+    imgCon.save(os.path.join(os.getcwd(),path_offset,"floor_{}_max_map.jpg".format(floorId) ) )
+    leg.save(os.path.join(os.getcwd(),path_offset,"floor_{}_max_legend.jpg".format(floorId) ) )
     #Minimum strength map
     cur.execute("""select x,y,min(strength) from location 
         join accesspoint on location.id=location_id
         where floor_id=%s
         group by location.id""",[floorId])
-    overlay_image(img,cur.fetchall()).show()
+    imgCon, leg = overlay_image(img,cur.fetchall(),-120)
+    imgCon.save(os.path.join(os.getcwd(),path_offset,"floor_{}_min_map.jpg".format(floorId) ) )
+    leg.save(os.path.join(os.getcwd(),path_offset,"floor_{}_min_legend.jpg".format(floorId) ) )
     #Mean strength map
-    cur.execute("""select x,y,min(strength)/count(*) from location 
+    cur.execute("""select x,y,sum(strength)/count(*) from location 
         join accesspoint on location.id=location_id
         where floor_id=%s
         group by location.id""",[floorId])
-    overlay_image(img,cur.fetchall()).show()
+    imgCon, leg = overlay_image(img,cur.fetchall(),-120)
+    imgCon.save(os.path.join(os.getcwd(),path_offset,"floor_{}_median_map.jpg".format(floorId) ) )
+    leg.save(os.path.join(os.getcwd(),path_offset,"floor_{}_median_legend.jpg".format(floorId) ) )
   
 
 
