bac5dcd0ce71dbb0d7e7b52f18dc1dac6b6d0d5a
bfisch02 <brett.fischler@tufts.edu>
2015-03-17 12:00:02 -0400

    Added comments and reformatted

--- kNN.py
+++ kNN.py
@@ -9,41 +9,48 @@ import sys
 import os
 from math import isinf
 
-# Minimum normalized RSSI value detected; used as "not detected" value
-MIN_DETECTED = 0
+
+#-------------
+# CONSTANTS
+#-------------
+
 COEFF_JACCARD = 3
 COEFF_EUCLIDEAN = 1
 COEFF_DENSITY = .112
 
+# Minimum normalized RSSI value detected; used as "not detected" value
+MIN_DETECTED = 0
+
+MAXINT = sys.maxint
+MININT = (MAXINT * -1) - 1
+
+# TODO: Either incorporate this or get rid of it
 MAC_COUNTS = {}
 
-#########################
-### CLASS DEFINITIONS ###
-#########################
+#---------------------
+# CLASS DEFINITIONS 
+#---------------------
 
-# Access Point class
 class AccessPoint(object):
+    """ AccessPoint Object """
     def __init__(self, ap, from_django=False):
 
         if not from_django:
             self.mac = ap[0]
             self.strength_dbm = float(ap[1])
-            self.strength = self.strength_dbm
-            #self.strength = 10 ** (float(ap[1]) / 10)
+            self.strength = 10 ** (float(ap[1]) / 10)
             self.std = 10 ** (float(ap[2]) / 10)
             self.datetime = ap[3]
 
         else:
             self.mac = ap['mac_address']
             self.strength_dbm = ap['signal_strength']
-            self.strength = self.strength_dbm
-            #self.strength = 10 ** (self.strength_dbm / 10)
+            self.strength = 10 ** (self.strength_dbm / 10)
             self.std = 10 ** (ap['standard_deviation'] / 10)
             self.datetime = ap['recorded']
 
-# Location Class
-# TODO: Look into storing previous distance calculations
 class Location(object):
+    """ Location Object """
     def __init__(self, loc):
         self.x = loc[0]
         self.y = loc[1]
@@ -51,23 +58,29 @@ class Location(object):
         self.floor_id = loc[3]
         self.init_aps(loc[4])
 
-    def printLoc(self):
-        sys.stdout.write("Location: (x, y) = (" + str(self.x) + ", " + str(self.y) + \
-                "), Floor = " + str(self.floor_id) + "\n")
-
     # Stores Access Points in a {mac_id : AccessPoint} dictionary
     def init_aps(self, aps):
+        """ Stores the AccessPoint list in a {mac_id : AccessPoint } dict """
         self.aps = {}
         for ap in aps:
             self.aps[ap[0]] = AccessPoint(ap)
 
 
-##########################
-### DISTANCE FUNCTIONS ###
-##########################
+#-------------------
+# DEBUGGING TOOLS
+#-------------------
 
-# Returns a set of shared keys between the two given AP dictionaries
+def printToFile():
+    return
+
+
+
+#----------------------
+# DISTANCE FUNCTIONS
+#----------------------
+
 def getSharedKeys(aps1, aps2):
+    """ Returns a set of shared keys between the two given AP dictionaries """
     keys = sets.Set()
     for mac_id in aps1.keys():
         keys.add(mac_id)
@@ -75,20 +88,20 @@ def getSharedKeys(aps1, aps2):
         keys.add(mac_id)
     return keys
 
-#Calculates distance between this Location and the given dictionary of
-#AccessPoints (currently calls function to calculate Euclidean distance)
 def kNNDistance(aps1, aps2, density = 0):
+    """ Returns distance between the given AccessPoint dicts.
+    Takes Jaccard coefficient, Euclidean distance, and density into account.
+    """
     distances = []
     euc_dist = euclidean(aps1, aps2)
-    jaccard = jaccardDistance(aps1, aps2)
-    if jaccard == 0:
+    jaccard_dist = jaccard(aps1, aps2)
+    if jaccard_dist == 0:
         return float("INF")
-    #return 1 / percent_shared + 1.5 * euc_dist
-    return (COEFF_JACCARD / jaccard) + (COEFF_EUCLIDEAN * euc_dist)  + (COEFF_DENSITY * density)
+    return (COEFF_JACCARD / jaccard_dist) + (COEFF_EUCLIDEAN * euc_dist) +\
+            (COEFF_DENSITY * density)
 
-# Given two dictionaries of AccessPoints, calculates the
-# Euclidean distance between the two dictionaries
 def euclidean(aps1, aps2):
+    """ Returns the Euclidean distance between the given AccessPoint dicts """
     global MIN_DETECTED
     keys = getSharedKeys(aps1, aps2)
     rVal = 0
@@ -102,7 +115,8 @@ def euclidean(aps1, aps2):
         rVal = rVal + ((strength1 - strength2) ** 2)
     return math.sqrt(rVal)
 
-def jaccardDistance(aps1, aps2):
+def jaccard(aps1, aps2):
+    """ Returns the Jaccard coeff between the given AccessPoint dicts """
     count = 0
     for ap in aps2.values():
         if ap.mac in aps1.keys():
@@ -113,16 +127,21 @@ def jaccardDistance(aps1, aps2):
 
 
 def realDistance(d1, d2):
+    """ Returns the real distance between the two given Location objects """
     if d1 is None or d2 is None:
         return 0
     return math.sqrt(pow(d1.x - d2.x, 2) + pow(d1.y - d2.y, 2))
 
 
-# Given a list of tuples where t[0] is the value and t[1] is the distance,
-# returns a weighted average of the values
+#-----------------
+# kNN FUNCTIONS
+#-----------------
+
 def weighted_avg(tuples, inverse):
-    ### If we want the unweighted average:
-    #return sum([t[0] for t in tuples]) / len(tuples)
+    """ Given a list of tuples (t[0] is the value and t[1] is the distance),
+    returns a weighted average of the values. If inverse == True, we use the
+    inverse of the given weights.
+    """
     s = 0
     for t in tuples:
         if t[1] == 0:
@@ -133,7 +152,6 @@ def weighted_avg(tuples, inverse):
         weight_sum = sum([t[1] for t in tuples])
     for t in tuples:
         if isinf(t[1]) or weight_sum == 0:
-            #print t[0]
             return t[0]
         if inverse:
             s += t[0] * (1 / t[1]) / weight_sum
@@ -141,39 +159,39 @@ def weighted_avg(tuples, inverse):
             s += t[0] * t[1] / weight_sum
     return s
 
-
-
-# Uses k - Nearest Neighbor technique to get the coordinates associated with
-# the given AccessPoint dictionary
-def apply_kNN(data, aps, k = 4, element = None):
+def applykNN(data, aps, k = 4, element = None):
+    """ Uses kNN technique to locate the given AccessPoint dict """
     k = min(k, len(data))
     floor = getFloor(data, aps)
     for d in data:
         if d.floor_id == floor:
             d.distance = kNNDistance(d.aps, aps, density=d.density)
-            #d.distance = euclidean(d.aps, aps)
-            #d.distance = 1 / jaccardDistance(d.aps, aps)
         else:
             d.distance = float("INF")
     data = sorted(data, key=lambda x: x.distance)
     #data = sorted(data, key=lambda x: realDistance(element, x))
     #for d in data:
-    #   print jaccardDistance(aps, d.aps), "->", euclidean(aps, d.aps), "->", kNNDistance(aps, d.aps, density=d.density), "->", d.density, "->", realDistance(d, element)
+    #   print jaccard(aps, d.aps), "->", euclidean(aps, d.aps), "->", kNNDistance(aps, d.aps, density=d.density), "->", d.density, "->", realDistance(d, element)
     x = weighted_avg([(loc.x, loc.distance) for loc in data[:k]], True)
     y = weighted_avg([(loc.y, loc.distance) for loc in data[:k]], True)
     return (x, y, floor, data[:k])
 
 def getFloor(data, aps, k = 5):
+    """ Uses kNN technique to find the floor of the given AccessPoint dict """
     k = min(k, len(data))
-    data = sorted(data, key=lambda d: jaccardDistance(d.aps, aps), reverse=True)
+    data = sorted(data, key=lambda d: jaccard(d.aps, aps), reverse=True)
     d = Counter([loc.floor_id for loc in data[:k]])
     floor = d.most_common(1)[0][0]
     return floor
 
-# Returns a list of Locations and an AccessPoint dictionary
-def get_locations(data):
+
+#----------------------
+# GET DATA FUNCTIONS
+#----------------------
+
+def getLocations(data):
+    """ Returns an array of Location objects corresponding to the given data"""
     locations = []
-    #sys.stderr.write("LENGTH: " + str(len(data)) + "\n")
     for d in data:
         cur_macs = d["macs"]
         cur_rss = d["rss"]
@@ -182,10 +200,6 @@ def get_locations(data):
             cur_aps.append((cur_macs[i], cur_rss[i], 0, 0))
         locations.append((d["x"], d["y"], d["direction"], d["floor_id"], cur_aps))
     return [Location(i) for i in locations]
-
-##########################
-### GET DATA FUNCTIONS ###
-##########################
 
 def getData(db_cursor=None):
     if db_cursor is None:
@@ -222,26 +236,26 @@ def getData(db_cursor=None):
     return res
 
 
-###############################
-### NORMALIZATION FUNCTIONS ###
-###############################
+#---------------------------
+# NORMALIZATION FUNCTIONS
+#---------------------------
 
-# Returns the standard deviation of the given list
 def get_sd(l):
+    """ Returns the standard deviation of the given list """
     mean = get_mean(l)
     rVal = 0
     for elem in l:
         rVal += (elem - mean) ** 2
     return (rVal / (len(l) - 1)) ** .5
 
-# Returns the mean of the given list
 def get_mean(l):
+    """ Returns the mean of the given list """
     return sum(l) / len(l)
 
-# BRETT NORMALIZE FUNCTION
-def normalize_all_data(data, testdata):
+def normalize(data):
+    """ Normalizes the given data and returns the mean and standard dev """
     global MIN_DETECTED
-    global MAC_COUNTS
+    global MAC_COUNTS # TODO: Get rid of this if we don't incorporate it
     strengths = []
     for loc in data:
         for ap in loc.aps.values():
@@ -256,46 +270,21 @@ def normalize_all_data(data, testdata):
             ap.strength = (ap.strength - mean) / st_dev
             if ap.strength < MIN_DETECTED:
                 MIN_DETECTED = ap.strength
-    for loc in testdata:
-        for ap in loc.aps.values():
-            ap.strength = (ap.strength - mean) / st_dev
-            if ap.mac not in MAC_COUNTS.keys():
-                MAC_COUNTS[ap.mac] = 0
-            MAC_COUNTS[ap.mac] += 1
+    return (mean, st_dev)
 
-def normalize_all_data2(data, testdata):
-    global MIN_DETECTED
-    global MAC_COUNTS
-
-    minstrength = sys.maxint
-    maxstrength = -1 * sys.maxint - 1
-    strengths = []
-    for loc in data:
-        for ap in loc.aps.values():
-            strengths.append(ap.strength)
-            if ap.strength < minstrength:
-                minstrength = ap.strength
-            if ap.strength > maxstrength:
-                maxstrength = ap.strength
-            if ap.mac not in MAC_COUNTS.keys():
-                MAC_COUNTS[ap.mac] = 0
-            MAC_COUNTS[ap.mac] += 1
-    for loc in data:
-        for ap in loc.aps.values():
-            ap.strength = (ap.strength - minstrength) / (maxstrength - minstrength)
-            if MIN_DETECTED > ap.strength:
-                MIN_DETECTED = ap.strength
-    for loc in testdata:
-        for ap in loc.aps.values():
-            ap.strength = (ap.strength - minstrength) / (maxstrength - minstrength)
+def normalizeAPs(aps, mean, st_dev):
+    """ Normalizes the given AccessPoint dict """
+    for ap in aps.values():
+        ap.strength = (ap.strength - mean) / st_dev
 
 
-##########################
-### ANALYSIS FUNCTIONS ###
-##########################
+#----------------------
+# ANALYSIS FUNCTIONS
+#----------------------
 
 def error(element, x, y, floor):
-    if element.floor_id == 6 and floor != 2:
+    """ Returns the error between the given element and our x and y vals """
+    if element.floor_id == 7 and floor != 2:
         return -1
     elif element.floor_id == 15 and floor != 1:
         return -1
@@ -306,6 +295,7 @@ def error(element, x, y, floor):
     return dist
 
 def addDensities(data):
+    """ Adds the density of points around each Location as a Location member """
     for i in range(len(data)):
         count = 1
         loc1 = data[i]
@@ -321,18 +311,29 @@ def addDensities(data):
                 count += 1
         loc1.density = count
 
-def testAccuracy(data, testdata):
+def testAccuracy():
+    """ Pulls data from the database, runs kNN on each test point, and prints
+    results to various files
+    """
+    sql_data = getData()
+    all_data = getLocations(sql_data)
+    data = []
+    testdata = []
+    for d in all_data:
+        if d.floor_id < 3:
+            data.append(d)
+        else:
+            testdata.append(d)
+    addDensities(data)
+    (mean, st_dev) = normalize(data)
     wrong_floor_count = 0
     error_total = 0
     distances = [0] * 10 # [0-1 meter, 1-2, 2-3, etc]
-    # 7, 8, 12, 16
-    #our_points = [2, 4, 6, 7, 13, 17]
-    our_points = range(19)
-    testdata = [d for (i, d) in enumerate(testdata) if i in our_points]
     for i in range(len(testdata)):
         element = testdata[i]
         aps = element.aps
-        (x, y, floor, neighbors)  = apply_kNN(data, aps, element = element)
+        normalizeAPs(aps, mean, st_dev)
+        (x, y, floor, neighbors)  = applykNN(data, aps, element = element)
         cur_error = error(element, x, y, floor)
         if cur_error == -1:
             print "Wrong floor"
@@ -360,30 +361,4 @@ def testAccuracy(data, testdata):
     return float(error_total) / len(testdata)
 
 if __name__ == "__main__":
-    sql_data = getData()
-    all_data = get_locations(sql_data)
-    data = [d for d in all_data if d.floor_id < 3]
-    testdata = [d for d in all_data if d.floor_id == 7]
-    addDensities(data)
-    normalize_all_data(data, testdata)
-    testAccuracy(data, testdata)
-    sys.exit(1)
-    global COEFF_DENSITY
-    global COEFF_JACCARD
-    COEFF_DENSITY = 0
-    COEFF_JACCARD = 3
-    best_density = 0
-    best_jaccard =0
-    best_val = 100
-    for i in range(1):
-        COEFF_DENSITY = .1
-        for j in range(10):
-            cur_error = testAccuracy(data, testdata)
-            if cur_error < best_val:
-                best_density = COEFF_DENSITY
-                best_jaccard = COEFF_JACCARD
-                best_val = cur_error
-            COEFF_DENSITY += .004
-        COEFF_JACCARD += .2
-    print "BEST ERROR:", best_val
-    print "BEST COEFFS:", best_jaccard, best_density
+    testAccuracy()
